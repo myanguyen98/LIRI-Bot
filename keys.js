d6ef4509d06c93a6d1c1c1b3bf539417078afcd5
myanguyen98 <waddio98@gmail.com>
2017-08-30 18:07:05 -0600

    Final commit

---
+++ keys.js
@@ -0,0 +1,13 @@
+//console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'ghWcoLNTMzVV4jmluselAzU6v',
+  consumer_secret: '5aBSLLVVSTJH79Mga3LokcqFNUo5XncKjIea1T2MYCqjhxCY0I',
+  access_token_key: '900893072799580165-KvgpOLQptex4GTYJ6oD8jxslWCXZ8E4',
+  access_token_secret: 'hYJ3UmqC0NPr2D6ABkh6kZTEDsKBSVlZKNuLqqK0sTI8a',
+}
+
+exports.Spotify = {
+			  id: "14371b3e01ff48fca6d95f4263c3e0e8",
+			  secret: "8cba0c8b6cc947eaacc2dedd6c13ba1c"
+			};
--- liri.js
+++ liri.js
@@ -1,41 +1,150 @@
 var request = require("request");
 var inquirer = require("inquirer");
+var keys = require("./keys.js")
+var Twitter = require('twitter');
+var Spotify = require('node-spotify-api');
 
-	var nodeArgs = process.argv;
-	var search = "";
-	for (var i = 2; i < nodeArgs.length; i++) {
-		search = search + " " + nodeArgs[i];
-	}
+//Main inquirer function to start the whole app
+//----------------------------------------------------------------------------------------------------
+inquirer.prompt([
+		{
+			type:"list",
+			name:"master",
+			message:"Welcome to my Node LIRI-Bot. Please choose a subject to get started.",
+			choices:["Twitter","Spotify","Movies"]
+		}])
 
+		.then(function(inquirerResponse){
+			if (inquirerResponse.master === "Twitter") {
+				twitterPrompt();
+			} else if(inquirerResponse.master === "Spotify"){
+				spotifyPrompt();
+			} else if(inquirerResponse.master === "Movies"){
+				moviePrompt();
+			};
+		})
+//Spotify commands
+//-------------------------------------------------------------------------------------------------
+function spotifyPrompt(){
+	var spotifyArgs = process.argv;
+		var spotifySearch = "";
+		for (var i = 2; i < spotifyArgs.length; i++) {
+			spotifySearch = spotifySearch + " " + spotifyArgs[i];
+		}
+
+	 inquirer.prompt([
+		{
+			type:"input",
+			name:"spotify",
+			message:"Please enter the name of a song and watch the command line display information for that track."
+		}])
+
+		.then(function(inquirerResponse){
+			spotifySearch = inquirerResponse.spotify;
+			displaySpotify();
+		})
+
+	function displaySpotify(){ 
+		
+	 
+				var spotify = new Spotify(keys.Spotify);
+				 
+				spotify.search({ type: 'track', query: spotifySearch }, function(err, data) {
+				  if (err) {
+				    return console.log('Error occurred: ' + err);
+				  }
+				  var song = data.tracks.items[0]
+				  console.log("\n--------------------------------------------------------"+
+				  	"\nSong Name: "+song.name+
+				  	"\n--------------------------------------------------------"+
+				  	"\nArtist: "+song.artists[0].name+
+				  	"\n--------------------------------------------------------"+
+				  	"\nLink to song on Spotify: "+song.external_urls.spotify+
+				  	"\n--------------------------------------------------------"+
+				  	"\nOriginating Album: "+song.album.name+
+				  	"\n--------------------------------------------------------")
+					
+				});
+
+	};
+};
+//Twitter commands
+//------------------------------------------------------------------------------------
+
+
+
+function twitterPrompt(){
+	var twitterSearch = "";
 	inquirer.prompt([
-	{
-		type:"input",
-		name:"movie",
-		message:"Welcome to my OMDB LIRI-Bot. Please enter the name of a movie and watch the command line display information for that film."
-	}])
+		{
+			type:"input",
+			name:"twitter",
+			message:"Please enter a twitter username (no spaces) and watch the command line display the user's latest tweets."
+		}])
 
-	.then(function(inquirerResponse){
-		search = inquirerResponse.movie;
-		display();
-	})
+		.then(function(inquirerResponse){
+			twitterSearch = inquirerResponse.twitter;
+			displayTweets();
+		})
+
+	function displayTweets(){
+
+		var client = new Twitter(keys.twitterKeys
+		);
+		 
+		var params = {screen_name: twitterSearch };
+		client.get('statuses/user_timeline', params, function(error, tweets, response) {
+		  if (!error) {
+
+		    for (var i = 0; i < tweets.length; i++) {
+		    	console.log("Tweet Posting Date: "+tweets[i].created_at+
+		    		"\nTweet Contents: "+tweets[i].text+"\n--------------------------------------------------------");
+		    }
+		  }
+		});
+	};
+};
+
+//Movie commands
+//-------------------------------------------------------------------------------------------------
+	
+function moviePrompt(){
+		var nodeArgs = process.argv;
+		var search = "";
+		for (var i = 2; i < nodeArgs.length; i++) {
+			search = search + " " + nodeArgs[i];
+		}
+
+		inquirer.prompt([
+		{
+			type:"input",
+			name:"movie",
+			message:"Please enter the name of a movie and watch the command line display information for that film."
+		}])
+
+		.then(function(inquirerResponse){
+			search = inquirerResponse.movie;
+			displayMovies();
+		})
 
 
 
 
-function display(){
-request("http://www.omdbapi.com/?t="+search+"&y=&plot=short&apikey=40e9cece", function(error, response, body) {
+	function displayMovies(){
+		request("http://www.omdbapi.com/?t="+search+"&y=&plot=short&apikey=40e9cece", function(error, response, body) {
 
-  
-  if (!error && response.statusCode === 200) {
+		  
+		  if (!error && response.statusCode === 200) {
 
-    console.log("Here is some information for "+search+":");
-    console.log("\nRelease Year: "+JSON.parse(body).Year+
-    	"\nIMDB Rating: "+JSON.parse(body).imdbRating+" out of 10 stars"+
-    	"\nRotten Tomatoes Score: "+JSON.parse(body).Ratings[1].Value+
-    	"\nProduction Country: "+JSON.parse(body).Country+
-    	"\nLanguage: "+JSON.parse(body).Language+
-    	"\nCast: "+JSON.parse(body).Actors+
-    	"\nBrief Summary: "+JSON.parse(body).Plot);
-  }
-});
+		    console.log("Here is some information for "+search+":");
+		    console.log("\nRelease Year: "+JSON.parse(body).Year+
+		    	"\nIMDB Rating: "+JSON.parse(body).imdbRating+" out of 10 stars"+
+		    	"\nRotten Tomatoes Score: "+JSON.parse(body).Ratings[1].Value+
+		    	"\nProduction Country: "+JSON.parse(body).Country+
+		    	"\nLanguage: "+JSON.parse(body).Language+
+		    	"\nCast: "+JSON.parse(body).Actors+
+		    	"\nBrief Summary: "+JSON.parse(body).Plot);
+		  }
+		});
+	};
 };
